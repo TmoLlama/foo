7829ce4d82e7f61e61dffa7db4f894ce64394b18
TmoLlama <TmoLlama@users.noreply.github.com>
2017-12-23 15:21:30 -0500

    sweet! we've got some tiles

---
+++ resources/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ultima v tiles" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="../../../../Pictures/desktopSlideshow/numbahonegameevahduh.PNG" width="1024" height="512"/>
+</tileset>
